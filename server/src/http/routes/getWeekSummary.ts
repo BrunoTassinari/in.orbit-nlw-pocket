2dde733a786545740f1be07ae03cff18a98a1fe3
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-09-15 01:13:54 -0300

    feat: get week summary

--- server/src/http/routes/getWeekSummary.ts
+++ server/src/http/routes/getWeekSummary.ts
@@ -1,10 +1,15 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { getWeekSummary } from '../../useCases/getWeekSummary';
 
-export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
-  app.get('/summary', async () => {
-    const { summary } = await getWeekSummary();
+export const getWeekSummaryRoute: FastifyPluginAsyncZod = async (app) => {
+  try {
+    app.get('/summary', async () => {
+      console.log('here');
+      const { summary } = await getWeekSummary();
 
-    return { summary };
-  });
+      return { summary };
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
