8928bf903b57623763afa148bd3c713b222d1255
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-09-15 23:31:07 -0300

    feat: create goal completion

--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -1,15 +1,25 @@
 import { Plus } from 'lucide-react';
 import { OutlineButton } from './ui/outline-button';
 import { GetPendingGoals } from '../http/get-pending-goals';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { createGoalCompletion } from '../http/create-goal-completion';
 
 export const PendingGoals = () => {
+  const queryClient = useQueryClient();
+
   const { data } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: GetPendingGoals,
   });
 
   if (!data) return null;
+
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId);
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] });
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] });
+  }
 
   return (
     <div className="flex flex-wrap gap-3">
@@ -18,6 +28,7 @@ export const PendingGoals = () => {
           <OutlineButton
             key={goal.id}
             disabled={goal.completion_count >= goal.desired_weekly_frequency}
+            onClick={() => handleCompleteGoal(goal.id)}
           >
             <Plus className="size-4 text-zinc-600" />
             {goal.title}
